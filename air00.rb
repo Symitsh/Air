9425d2a472128cf80ca86fd59aa5337128319b39
Symitsh <sandri822@hotmail.fr>
2022-03-30 15:07:01 +0200

    refacto

--- air00.rb
+++ air00.rb
@@ -5,16 +5,26 @@
 
 # Fonctions utilisées :
 def ma_fonction(string_à_couper, string_séparateur) 
-    tableau = string_à_couper[0]                                                   # 2: Puis zoom dans mon tableau pour avoir un string
-    tableau.each_line(string_séparateur, chomp: true) {|element| puts element}     # 3: Affiche chaque élément avec entre un séparateur
+    tableau = string_à_couper[0]                                        # 2: Puis zoom dans mon tableau pour avoir un string
+    cancel_separators(tableau, string_séparateur)
     return tableau
+end
+
+def cancel_separators(tableau, string_séparateur)                
+    separator = ""
+    tableau.each_line(string_séparateur, chomp: true) do |element|     # 3: Affiche chaque élément de mon tableau avec entre, un séparateur
+        if tableau[element] == separator                               # 4: Si séparateurs supplémentaires, passe aux caractères suivants
+            next
+        end
+        puts tableau[element]
+    end
 end
 
 # Partie 1: Gestion d'erreur
 (puts "error"; exit) if ARGV.size != 1
 
 # Partie 2 : Parsing
-string_à_couper = ARGV                                                              # 1: Transforme ma chaîne de caractère en tableau
+string_à_couper = ARGV                                                  # 1: Transforme ma chaîne de caractère en tableau
 string_séparateur = ' '
 
 # Partie 3 et 4: Résolution, Affichage
