e73edfc6ab839f0d702bf3bd842607b3f4586ed0
Symitsh <sandri822@hotmail.fr>
2022-04-03 13:10:42 +0200

    Ajout de modification ma_fonction

--- air02.rb
+++ air02.rb
@@ -1,35 +1,23 @@
-# ***** Concat *****
-# Créez un programme qui transforme un tableau de chaînes de caractères
-# en une seule chaîne de caractères. Espacés d’un séparateur donné en
-# dernier argument au programme.
-=begin
-
-$> python exo.py “je” “teste” “des” “trucs” “ “
-Je teste des trucs
-
-*********************************
-p solution me donne un tableau de string avec le séparateur à la fin
-combiné cette chaine en y rajoutant entre chaque mot le separateur n-1
-
-=end
-
-# Fonctions utilisées :
+#   ***** Concat *****
+# Ne pas utiliser .concat
+# Fonction utilisée :
 def ma_fonction(array_de_strings, separateur)
-    array_de_strings
+    string = ""                                     
+    array_de_strings.each do |element|              # Enlève chaque élement du tableau et push dans la string
+        string << (element + separateur)            # Avec entre chaque, le dernier élément du tableau
+    end
+    return string
 end
 
-
-
-
 # Partie 1: Gestion d'erreur
-(puts "error"; exit) if ARGV.size <= 2
+(puts "error"; exit) if ARGV.size <= 1
 
 # Partie 2 : Parsing
-array_de_strings = ARGV
-separateur = ' '
+array_de_strings = ARGV                                       
+separateur = ARGV.pop
 
 # Partie 3: Résolution
 solution = ma_fonction(array_de_strings, separateur)
 
-# Partie 4 : Affichage
-p solution
+# Partie 4: Affichage
+puts solution
--- air03.rb
+++ air03.rb
@@ -1,29 +1,17 @@
 # ***** Chercher l’intrus *****
-# Créez un programme qui retourne la valeur d’une liste qui n’a pas de paire.
-=begin
-Exemples d’utilisation :
-$> python exo.py 1 2 3 4 5 4 3 2 1
-5
-$> python exo.py
-**************************************
-Créer une boucle qui va parcourir chaque élément du tableau et vérifier si il y a une paire
-Si paire trouver la supprimer du tableau
-Si pas de paire l'afficher et return
-
-=end
 # Fonctions utilisées :
 
 
 
 
 # Partie 1: Gestion d'erreur
-(puts "error"; exit) if ARGV.size <= 2
+#(puts "error"; exit) if ARGV.size <= 2
 
 # Partie 2 : Parsing
 
 
 # Partie 3: Résolution
-solution = ma_fonction()
+
 
 # Partie 4 : Affichage
-p solution
+
