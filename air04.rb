8a54259a15d159736e037f214fa27034414da609
Symitsh <sandri822@hotmail.fr>
2022-04-04 15:10:38 +0200

    Ajout de ma_fonction

--- air04.rb
+++ air04.rb
@@ -1,28 +1,21 @@
 # ***** Un seul à la fois *****
-# Créez un programme qui aﬃche une chaîne de caractères en évitant les
-# caractères identiques adjacents.
-=begin
-Exemples d’utilisation :
-$> python exo.py "Hello milady,  bien ou quoi ??"
-Helo milady, bien ou quoi ?
-*********************************
-Crée une boucle qui parcour chaque éléments, si un élément égal le prochain élément, le supprime
-lettres, espace, points conjonction
-=end
-
 # Fonctions utilisées :
-
-
-
+def ma_fonction(array_de_strings)
+    array = []
+    for x in 0..array_de_strings.length
+        array << array_de_strings[x]
+        if array[x] != array[x-1]
+            print array[x-1]
+        end
+    end
+    puts ""
+end
 
 # Partie 1: Gestion d'erreur
 (puts "error"; exit) if ARGV.size != 1
 
 # Partie 2 : Parsing
+array_de_strings = ARGV[0]
 
-
-# Partie 3: Résolution
-solution = ma_fonction()
-
-# Partie 4 : Affichage
-p solution
+# Partie 3: Résolution / Affichage
+solution = ma_fonction(array_de_strings)
