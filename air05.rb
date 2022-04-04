bc3a66b8a06f5704c8559c82e279c024bcc5c051
Symitsh <sandri822@hotmail.fr>
2022-04-04 21:24:41 +0200

    Ajout de ma_fonction

--- air05.rb
+++ air05.rb
@@ -1,28 +1,19 @@
 # ***** Sur chacun d’entre eux *****
 # Fonctions utilisées :
-def ma_fonction(array_de_strings, string_opération)
+def ma_fonction(array_de_strings)
     array = []
-    array << array_de_strings[-1] ; array_de_strings.pop
-    array_de_strings.map do |element|
-        solu = element.to_i + string_opération
-        puts solu
-    end
+    array << array_de_strings[-1] ; array_de_strings.pop                    # Transfert le dernier élément de mon tableau et le supprime
+    return array_de_strings.map { |element| element.to_i + array[0].to_i }
 end
 
 # Partie 1: Gestion d'erreur
 (puts "error"; exit) if ARGV.size <= 1
 
 # Partie 2 : Parsing
-array_de_strings = ARGV                                                 # 1: Transforme ma chaîne de caractère en tableau
-string_opération = ARGV[-1].to_i
+array_de_strings = ARGV                               
 
 # Partie 3: Résolution
-solution = ma_fonction(array_de_strings, string_opération)
+solution = ma_fonction(array_de_strings)
 
-=begin
-    
-    array_de_strings.each do |element|
-        print element.to_i + string_opération
-    end
-    
-=end
+# Partie 4: Solution
+puts solution.join(' ').split(', ')
