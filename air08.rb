66949cdbb56200ae18a0d484c9734db2e2fbdab1
Symitsh <sandri822@hotmail.fr>
2022-04-05 11:51:12 +0200

    Ajout de la fonction sorted_fusion(array1, array2)

--- air08.rb
+++ air08.rb
@@ -1,8 +1,16 @@
 # ***** Mélanger deux tableaux triés *****
 # Fonction utilisée :
 def sorted_fusion(array1, array2)
-
-
+    new_array = array1 + array2
+    1.upto(new_array.length-1) do |i|
+        value = new_array[i]
+        j = i - 1
+        while j >= 0 and new_array[j] > value
+            new_array[j+1] = new_array[j]
+            j -= 1
+        end
+        new_array[j+1] = value
+    end
     return new_array
 end
 
@@ -10,6 +18,7 @@ end
 (puts "error"; exit) if ARGV.size <= 2
 
 # Partie 2 : Parsing
+p array = ARGV
 array1 =
 array2 =
 
@@ -18,5 +27,3 @@ solution = sorted_fusion(array1, array2)
 
 # Partie 4: Solution
 puts solution
-
-
