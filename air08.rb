63808820b5d8b81e46fd4f0c61e9b8c56aad088b
Symitsh <sandri822@hotmail.fr>
2022-04-05 16:09:15 +0200

    Modification Parsing

--- air08.rb
+++ air08.rb
@@ -1,7 +1,7 @@
 # ***** Mélanger deux tableaux triés *****
 # Fonction utilisée :
 def sorted_fusion(array1, array2)
-    new_array = array1 + array2
+    new_array = array1 + array2 # array1.union([array2])
     1.upto(new_array.length-1) do |i|
         value = new_array[i]
         j = i - 1
@@ -18,15 +18,14 @@ end
 (puts "error"; exit) if ARGV.size <= 2
 
 # Partie 2 : Parsing
-p array = ARGV
+array = ARGV
 array1, array2 = [], []
 fusion = array.index("fusion")
-array2 = array.delete_if.with_index {|element, index| index >= 0 && index <= fusion}
-array1 = array.delete_if.with_index {|element, index| index >= fusion && index <= fusion*2}
-# array.each.with_index {|element, index| (index < fusion) ? (array1 << element) : (array2 << element)} 
+array1 = array.values_at(0..fusion-1)       # Retourne un nouveau tableau allant de l'index 0 à l'index avant 'fusion'
+array2 = array.values_at(fusion+1..-1)      # Retourne un nouveau tableau allant de l'index après 'fusion' au dernier index
 
 # Partie 3: Résolution
 solution = sorted_fusion(array1, array2)
 
 # Partie 4: Solution
-# puts solution
+puts solution.join(' ')
