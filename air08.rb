e03e1e4e2686574d495e90408746e67ea291381e
Symitsh <sandri822@hotmail.fr>
2022-04-05 11:41:16 +0200

    Création du programme Mélanger deux tableaux triés

---
+++ air08.rb
@@ -0,0 +1,22 @@
+# ***** Mélanger deux tableaux triés *****
+# Fonction utilisée :
+def sorted_fusion(array1, array2)
+
+
+    return new_array
+end
+
+# Partie 1: Gestion d'erreur
+(puts "error"; exit) if ARGV.size <= 2
+
+# Partie 2 : Parsing
+array1 =
+array2 =
+
+# Partie 3: Résolution
+solution = sorted_fusion(array1, array2)
+
+# Partie 4: Solution
+puts solution
+
+
