3fb1d9d5ee4389cb3e44d112d9a2aa3546786ba4
Symitsh <sandri822@hotmail.fr>
2022-04-05 08:47:15 +0200

    Création du programme Insertion dans un tableau trié

---
+++ air07.rb
@@ -0,0 +1,19 @@
+# ***** Insertion dans un tableau trié *****
+# Fonction utilisée
+def sorted_insert(array, new_element)
+
+    # return new_array
+end
+
+# Partie 1: Gestion d'erreur
+(puts "error"; exit) if ARGV.size <= 2
+
+# Partie 2 : Parsing
+array = ARGV   
+new_element = ARGV[-1]
+
+# Partie 3: Résolution
+solution = ma_fonction(array, new_element)
+
+# Partie 4: Solution
+puts solution
