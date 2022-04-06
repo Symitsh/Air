a658e7913cb17d520cceedc36a3d93061b957a5f
Symitsh <sandri822@hotmail.fr>
2022-04-06 10:03:16 +0200

    Ajout de la fonction pyramid(symbol, height

--- air11.rb
+++ air11.rb
@@ -1,17 +1,17 @@
 # ***** Afficher une pyramide *****
 # Fonction utilisée :
-def ma_fonction()
-
+def pyramid(symbol, height)
+    height.times {|element|
+        print ' ' * (height - element) 
+        puts symbol * (2 * element + 1)}
 end
 
 # Partie 1: Gestion d'erreur
-(puts "error"; exit) if ARGV.size > 1
+(puts "error"; exit) if ARGV.size != 2
 
 # Partie 2 : Parsing
+symbol = ARGV[0]
+height = ARGV[1].to_i
 
-
-# Partie 3: Résolution
-solution = ma_fonction()
-
-# Partie 4: Solution
-puts solution
+# Partie 3: Résolution et affichage
+solution = pyramid(symbol, height)
