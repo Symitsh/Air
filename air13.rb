5e3e07d15dda3f9595c67de08c8e1ba991becef0
Symitsh <sandri822@hotmail.fr>
2022-04-06 18:18:26 +0200

    Ajout de modificateur de couleur

--- air13.rb
+++ air13.rb
@@ -1,15 +1,18 @@
 # ***** Meta exercice *****
 # Fonction utilisée:
+def green(text)  "\e[32m#{text}\e[0m"  end
+def red(text)    "\e[31m#{text}\e[0m"  end
 
 
 
 
 
 # Partie 1: Gestion d'erreur
-(puts "error"; exit) if ARGV.size != 0
+#(puts "error"; exit) if ARGV.size != 0
 
 # Partie 2 : Parsing
-
+puts green("Bonjour")
+puts red("Super")
 
 # Partie 3: Résolution
 
