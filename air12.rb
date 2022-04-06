de8f2edeb0229c3f1e830a4e8e1799be96e1fc76
Symitsh <sandri822@hotmail.fr>
2022-04-06 11:00:50 +0200

    Ajout de la méthode my_quick_sort(arr)

--- air12.rb
+++ air12.rb
@@ -1,13 +1,17 @@
 # ***** Le roi des tris *****
-# Fonction utilisée
-def my_quick_sort(array)
-    p array
+# Fonction utilisée:
+def my_quick_sort(arr)
+    new_array = arr
+    return new_array if new_array.size < 2
 
-    # return new_array
+    pivot = new_array[0]
+    less = new_array[1..].select {|element| element <= pivot}
+    greater = new_array[1..].select {|element| element > pivot}
+    return *my_quick_sort(less), pivot, *my_quick_sort(greater)
 end
 
 # Partie 1: Gestion d'erreur
-(puts "error"; exit) if ARGV.size <= 2
+(puts "error"; exit) if ARGV.size <= 1
 
 # Partie 2 : Parsing
 array = ARGV
