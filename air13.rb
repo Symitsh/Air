ffbd96f13d4aeb43ada4f67b81d375fbf79e98ee
Symitsh <sandri822@hotmail.fr>
2022-04-06 11:06:01 +0200

    Création du programme Meta exercice

---
+++ air13.rb
@@ -0,0 +1,18 @@
+# ***** Meta exercice *****
+# Fonction utilisée:
+
+
+
+
+
+# Partie 1: Gestion d'erreur
+(puts "error"; exit) if ARGV.size != 0
+
+# Partie 2 : Parsing
+
+
+# Partie 3: Résolution
+
+
+# Partie 4: Solution
+
