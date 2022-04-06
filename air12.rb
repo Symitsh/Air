a1b0ebb14d0031f9aa320b8a5542e55eed560f24
Symitsh <sandri822@hotmail.fr>
2022-04-06 10:07:12 +0200

    Création du programme Le roi des tris

---
+++ air12.rb
@@ -0,0 +1,19 @@
+# ***** Le roi des tris *****
+# Fonction utilisée
+def my_quick_sort(array)
+    p array
+
+    # return new_array
+end
+
+# Partie 1: Gestion d'erreur
+(puts "error"; exit) if ARGV.size <= 2
+
+# Partie 2 : Parsing
+array = ARGV
+
+# Partie 3: Résolution
+solution = my_quick_sort(array)
+
+# Partie 4: Solution
+puts solution.join(' ')
