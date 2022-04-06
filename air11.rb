0d18475656ee3bc56d5bfc8887430d4e9ec4adf5
Symitsh <sandri822@hotmail.fr>
2022-04-06 09:25:12 +0200

    Création du programme Afficher une pyramide

---
+++ air11.rb
@@ -0,0 +1,17 @@
+# ***** Afficher une pyramide *****
+# Fonction utilisée :
+def ma_fonction()
+
+end
+
+# Partie 1: Gestion d'erreur
+(puts "error"; exit) if ARGV.size > 1
+
+# Partie 2 : Parsing
+
+
+# Partie 3: Résolution
+solution = ma_fonction()
+
+# Partie 4: Solution
+puts solution
