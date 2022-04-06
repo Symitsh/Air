6c0fc41750714ca69ec7f7dc0912ef89f73e1684
Symitsh <sandri822@hotmail.fr>
2022-04-06 18:24:24 +0200

    Ajout des méthodes meta + air{00..12}

--- air13.rb
+++ air13.rb
@@ -1,21 +1,97 @@
 # ***** Meta exercice *****
-# Fonction utilisée:
+# Fonctions utilisées:
 def green(text)  "\e[32m#{text}\e[0m"  end
 def red(text)    "\e[31m#{text}\e[0m"  end
+def meta
+
+    def air00
 
 
 
+    end
 
+    def air01
+
+
+
+    end
+
+    def air02
+
+
+
+    end
+
+    def air03
+
+
+
+    end
+
+    def air04
+
+
+
+    end
+
+    def air05
+
+
+
+    end
+
+    def air06
+
+
+
+    end
+
+    def air07
+
+
+
+    end
+
+    def air08
+
+
+
+    end
+
+    def air09
+
+
+
+    end
+
+    def air10
+
+
+
+    end
+
+    def air11
+
+
+
+    end
+
+    def air12
+
+
+
+    end
+end
 
 # Partie 1: Gestion d'erreur
-#(puts "error"; exit) if ARGV.size != 0
+(puts "error"; exit) if ARGV.size != 0
 
 # Partie 2 : Parsing
 puts green("Bonjour")
 puts red("Super")
 
 # Partie 3: Résolution
-
+solution = meta
 
 # Partie 4: Solution
-
+puts meta
