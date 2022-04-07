c5c89b8566dcf1d847aad2f59ff0889a63a03360
Symitsh <sandri822@hotmail.fr>
2022-04-07 11:54:19 +0200

    Ajout de gestion d'erreur

--- air13.rb
+++ air13.rb
@@ -2,17 +2,18 @@
 # Fonctions utilisées:
 def green(text)  "\e[32m#{text}\e[0m"  end
 def red(text)    "\e[31m#{text}\e[0m"  end
+
 def meta
 
     def air00
-
 
 
     end
 
     def air01
 
-
+        puts green("Bonjour")
+        puts red("Super")
 
     end
 
@@ -84,14 +85,19 @@ def meta
 end
 
 # Partie 1: Gestion d'erreur
+# Boucle qui vérifie si le fichier existe et si il est executable. Renvoie 'error' et quitte le programme dans le cas contrainre
+for i in '00'..'14'
+    (puts "error, air#{i}.rb exist?"; exit) if !File.exist?("air#{i}.rb")
+    (puts "error, file#{i} is executable ?"; exit) if !File.executable?("air#{i}.rb") 
+end
 (puts "error"; exit) if ARGV.size != 0
 
-# Partie 2 : Parsing
-puts green("Bonjour")
-puts red("Super")
+# Partie 2: Parsing
+
 
 # Partie 3: Résolution
 solution = meta
 
 # Partie 4: Solution
-puts meta
+#puts meta
+puts air01
