0a73cc04c1a1b3fa5c2da6829812111c49fed1fb
Sandri Florent <sandri822@hotmail.fr>
2022-04-09 21:15:49 +0200

    Refacto ajout de la class Meta_exercice

--- air13.rb
+++ air13.rb
@@ -1,11 +1,11 @@
 # ***** Meta exercice *****
-# Fonctions utilisées:
-def green(text)  "\e[32m#{text}\e[0m"  end
-def red(text)    "\e[31m#{text}\e[0m"  end
-def evaluate
-    def air00
+class Meta_exercice
+    # Méthodes utilisées:
+    def self.green(text) ; "\e[32m#{text}\e[0m"  end
+    def self.red(text)   ; "\e[31m#{text}\e[0m"  end
+    def self.air00
         output00 = `ruby air00.rb "Bonjour les gars"`
-        output00.each_line do |element| 
+        output00.each_line do |element|
             if element == "Bonjour\n" || element == "les\n" || element == "gars\n"
                 puts "air00 (#{$a += 1}/3) : #{$success}" ; $result += 1
             else
@@ -14,8 +14,8 @@ def evaluate
         end
         return
     end
-    
-    def air01
+
+    def self.air01
         $a = 0
         output01 = `ruby air01.rb "Crevette magique dans la mer des étoiles" "la"`
         output01.each_line do |element|
@@ -28,7 +28,7 @@ def evaluate
         return
     end
 
-    def air02
+    def self.air02
         $a = 0
         output02 = `ruby air02.rb "je" "teste" "des" "trucs" " "`
         output02.each_line do |element|
@@ -41,7 +41,7 @@ def evaluate
         return
     end
 
-    def air03
+    def self.air03
         $a = 0
         output03 = `ruby air03.rb 1 2 3 4 5 4 3 2 1`
         output03.each_line do |element|
@@ -62,7 +62,7 @@ def evaluate
         return
     end
 
-    def air04
+    def self.air04
         $a = 0
         output04 = `ruby air04.rb "Hello milady,  bien ou quoi ??"`
         output04.each_line do |element|
@@ -75,7 +75,7 @@ def evaluate
         return
     end
 
-    def air05
+    def self.air05
         $a = 0
         output05 = `ruby air05.rb 1 2 3 4 5 "+2"`
         output05.each_line do |element|
@@ -96,7 +96,7 @@ def evaluate
         return
     end
 
-    def air06
+    def self.air06
         $a = 0
         output06 = `ruby air06.rb "Michel" "Albert" "Thérèse" "Benoit" "t"`
         output06.each_line do |element|
@@ -117,7 +117,7 @@ def evaluate
         return
     end
 
-    def air07
+    def self.air07
         $a = 0
         output07 = `ruby air07.rb 1 3 4 2`
         output07.each_line do |element|
@@ -138,7 +138,7 @@ def evaluate
         return
     end
 
-    def air08
+    def self.air08
         $a = 0
         output08 = `ruby air08.rb 10 20 30 fusion 15 25 35`
         output08.each_line do |element|
@@ -151,7 +151,7 @@ def evaluate
         return
     end
 
-    def air09
+    def self.air09
         $a = 0
         output09 = `ruby air09.rb "Michel" "Albert" "Thérèse" "Benoit"`
         output09.each_line do |element|
@@ -164,7 +164,7 @@ def evaluate
         return
     end
 
-    def air10
+    def self.air10
         $a = 0
         output10 = `ruby air10.rb cat a.txt`
         output10.each_line do |element|
@@ -185,7 +185,7 @@ def evaluate
         return
     end
 
-    def air11
+    def self.air11
         $a = 0
         output11 = `ruby air11.rb O 5`
         output11.each_line do |element|
@@ -199,7 +199,7 @@ def evaluate
         return
     end
 
-    def air12
+    def self.air12
         $a = 0
         output12 = `ruby air12.rb 6 5 4 3 2 1`
         output12.each_line do |element|
@@ -211,29 +211,25 @@ def evaluate
         end
         return
     end
-    ("air00".."air12").each { |element| puts eval(element) } ; puts "Total success: (#{$result}/25)"
-end
 
-# Boucle qui vérifie si le fichier existe et si il est executable. Renvoie 'error' et quitte le programme dans le cas contrainre
-def error_if
-    for i in '00'..'14'
-        (puts "error, air#{i}.rb exist?") if !File.exist?("air#{i}.rb")
-        (puts "error, file#{i} is executable?") if !File.executable?("air#{i}.rb") 
+    # Boucle qui vérifie si le fichier existe et si il est executable. Renvoie 'error' et quitte le programme dans le cas contrainre
+    def self.error_if
+      for i in '00'..'14'
+          (puts "error, air#{i}.rb exist?") if !File.exist?("air#{i}.rb")
+          (puts "error, file#{i} is executable?") if !File.executable?("air#{i}.rb")
+      end
+      (puts "error argument"; exit) if ARGV.size != 0
     end
-    (puts "error argument"; exit) if ARGV.size != 0
 end
 
 # Partie 1: Gestion d'erreur
-error_if()
+Meta_exercice.error_if()
 
 # Partie 2: Parsing
-$success = green("success")
-$failure = red("failure")
+$success = Meta_exercice.green("success")
+$failure = Meta_exercice.red("failure")
 $a = 0
 $result = 0
 
-# Partie 3: Résolution
-solution = evaluate
-
-# Partie 4: Affichage
-puts solution
+# Partie 3: Résolution et Affichage
+("Meta_exercice.air00".."Meta_exercice.air12").each { |element| puts eval(element) } ; puts "Total success: (#{$result}/25)"
